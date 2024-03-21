e910852d5bd6694738805783030e7143385776f8
amit pandit <33733551+amipan@users.noreply.github.com>
2024-03-20 22:22:21 -0400

    in for loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
@@ -36,6 +37,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        #st.text(fruityvice_response.json() )
+
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)     
 
     #st.write(ingredients_string)
 
@@ -50,8 +55,5 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json() )
 
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
